823fdb5030d09d3c58f2f4f04fc7dd4ef386e586
Sarai Hannah Ajai <ajai.sarai.h@outlook.com>
2020-02-16 15:53:53 -0600

    Create Module1PrimitivesVSObjectsOnlineLectureExample2.java

---
+++ Module1PrimitivesVSObjectsOnlineLectureExample2.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+public class Module1PrimitivesVSObjectsOnlineLectureExample2 {
+    public static void main(String[] args) {
+        System.out.println("Java String Array Initialization after declaration.");
+        String[] strArray = new String[5];
+        strArray[0] = "C";
+        strArray[1] = "h";
+        strArray[2] = "a";
+        strArray[3] = "r";
+        strArray[4] = "s";
+        for (int i = 0; i < strArray.length; i++) {
+            System.out.print(strArray[i]);
+        }
+    }
+}
