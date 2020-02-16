7667ff0107bc2894217e6e3879e45d76080a3647
Sarai Hannah Ajai <ajai.sarai.h@outlook.com>
2020-02-16 15:53:59 -0600

    Create PrimitivesVSObjectsQuizQuestion1.java

---
+++ PrimitivesVSObjectsQuizQuestion1.java
@@ -0,0 +1,8 @@
+
+public class PrimitivesVSObjectsQuizQuestion1 {
+    public static void main(String[] args) {
+        String s1 = "hello";
+        System.out.println(s1);
+        System.out.println(s1.length());
+    }
+}
