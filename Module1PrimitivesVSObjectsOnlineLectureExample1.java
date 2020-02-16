e9ad583fdca8d6a6c9254b1ae5cd498c3d0c1351
Sarai Hannah Ajai <ajai.sarai.h@outlook.com>
2020-02-16 15:53:48 -0600

    Create Module1PrimitivesVSObjectsOnlineLectureExample1.java

---
+++ Module1PrimitivesVSObjectsOnlineLectureExample1.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Module1PrimitivesVSObjectsOnlineLectureExample1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("What is your next hint number? " );
+        int hintNumber = input.nextInt();
+        System.out.println("You have selected " + hintNumber + " for next hint number.");
+    }
+}
