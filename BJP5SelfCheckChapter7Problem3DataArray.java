9a6c352f92f9de63677eafe3fec976af9852bffc
Sarai Hannah Ajai <ajai.sarai.h@outlook.com>
2020-02-16 19:19:15 -0600

    Create BJP5SelfCheckChapter7Problem3DataArray.java
    BJP5 Self Check Homework Assignment for 7.3 Data Array and 7.5 from PracticeIt.cs.washington.edu Dated on 02-16-20

---
+++ BJP5SelfCheckChapter7Problem3DataArray.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+public class BJP5SelfCheckChapter7Problem3DataArray {
+    public static void main(String[] args) {
+
+        int[] data = {27, 51, 33, -1, 101};
+
+
+        for (int i = 0; i < data.length; i++) {
+            System.out.println(data[i]);
+        }
+
+    }
+}
