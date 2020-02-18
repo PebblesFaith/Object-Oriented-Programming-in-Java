461eec43e83c48ec807b299e44eeb784bdcf4e47
Sarai Hannah Ajai <ajai.sarai.h@outlook.com>
2020-02-17 19:13:38 -0600

    Create Module1CodingWithArrayOnlineNotesExample2.java
    Online notes practice Coding With Arrays for "Object Oriented Programming in Java (Microsoft Dev 277x) course dated on 02-17-20.

---
+++ Module1CodingWithArrayOnlineNotesExample2.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+public class Module1CodingWithArrayOnlineNotesExample2 {
+    public static void main(String[] args) {
+        int a = 10;
+        double aSq = Math.pow(a, 2) ;
+
+        int[] b = new int[10];
+        b[0] = 2;
+        b[1] = 4;
+        System.out.println(Arrays.toString(b));
+    }
+}
