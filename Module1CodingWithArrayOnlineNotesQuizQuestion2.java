1a34918393715b49f8cffa70e0a311cb5cdd2add
Sarai Hannah Ajai <ajai.sarai.h@outlook.com>
2020-02-17 19:15:11 -0600

    Create Module1CodingWithArrayOnlineNotesQuizQuestion2.java
    Online notes practice Coding With Arrays Quiz question number 2 for "Object Oriented Programming in Java (Microsoft Dev 277x) course dated on 02-17-20.

---
+++ Module1CodingWithArrayOnlineNotesQuizQuestion2.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+public class Module1CodingWithArrayOnlineNotesQuizQuestion2 {
+    public static void main(String[] arg) {
+        int[] a = new int[5];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = i;
+
+            System.out.println(a[i]);
+        }
+    }
+
+}
