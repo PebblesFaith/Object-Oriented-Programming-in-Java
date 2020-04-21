2aa2f09d5567ac49ab4915fa692b17b591bde9a4
Sarai Hannah Ajai <ajai.sarai.h@outlook.com>
2020-04-21 18:57:13 -0500

    Update BJP5SelfCheckChapter7Problem5NumberArray.java
    Object Oriented Programming in Java (Microsoft Dev 277x) course from Instructor, Kasey Champion's Practice-It! Assignment for BJP4 Self-Check 7.5 numbersArray. Completed by Sarai Hannah Ajai Dated on April 21, 2020.

--- BJP5SelfCheckChapter7Problem5NumberArray.java
+++ BJP5SelfCheckChapter7Problem5NumberArray.java
@@ -1,3 +1,7 @@
+/* Object Oriented Programming in Java (Microsoft Dev 277x) course from Instructor, Kasey Champion's
+   Practice-It! Assignment for BJP4 Self-Check 7.5 numbersArray.
+   Completed by Sarai Hannah Ajai Dated on April 21, 2020. */
+
 import java.util.Arrays;
 public class BJP5SelfCheckChapter7Problem5NumberArray {
     public static void main(String[] args) {
@@ -6,12 +10,12 @@ public class BJP5SelfCheckChapter7Problem5NumberArray {
         numbers[4] = 99;
         numbers[7] = 2;
 
-        int x = numbers[1];
+        int x = numbers[1]; // Numbers[1] index is equal to the value of 4 that is replace by Numbers[4] index location of the value of 44.
         numbers[x] = 44;
-        numbers[numbers[7]] = 11;
+        numbers[numbers[7]] = 11; // Numbers[7] index that is equal to the eleven (11) value is replace to the index value of two (2) location.
 
         for (int i = 0; i < numbers.length; i++) {
-            System.out.println(numbers[x]);
+            System.out.println(numbers[i]);
         }
     }
 }
