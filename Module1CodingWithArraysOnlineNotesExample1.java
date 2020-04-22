3b956e1d5bf1d8cdabb6f7bc4a2c6a127b62a433
Sarai Hannah Ajai <ajai.sarai.h@outlook.com>
2020-04-21 19:18:37 -0500

    Update Module1CodingWithArraysOnlineNotesExample1.java
    Module 1 | Coding With Arrays Example 1
    Completed by: Sarai Hannah Ajai
    on April 21, 2020

--- Module1CodingWithArraysOnlineNotesExample1.java
+++ Module1CodingWithArraysOnlineNotesExample1.java
@@ -1,3 +1,7 @@
+// Module 1 | Coding With Arrays Example 1
+// Completed by: Sarai Hannah Ajai
+// on April 21, 2020
+
 import java.util.Arrays;
 
 public class Module1CodingWithArraysOnlineNotesExample1 {
@@ -6,26 +10,6 @@ public class Module1CodingWithArraysOnlineNotesExample1 {
         double[] b = new double[10];
         char [] c = new char[20];
 
-        a[0] = 15;
-
-        b[0] = 10;
-        b[1] = 20;
-        b[2] = 30;
-        b[3] = 40;
-        b[4] = 50;
-
-        c[0] = 'a';
-        c[1] = 'b';
-        c[2] = 'c';
-        c[3] = 'd';
-        c[4] = 'e';
-        c[5] = 'f';
-        c[6] = 'g';
-        c[7] = 'h';
-        c[8] = 'i';
-        c[9] = 'j';
-        c[10] = 'l';
-        c[11] = 'k';
 
         int aLength = a.length; // returns 5
         int bLength = b.length; // returns 10
@@ -39,8 +23,8 @@ public class Module1CodingWithArraysOnlineNotesExample1 {
             System.out.println(b[i]);
         }
 
-        for (int i = 0; i < cLength; i++) {
-            System.out.println(c[i]);
+        for (int i = 0; i < c.length; i++) {
+            System.out.println(Arrays.toString(c));
         }
 
 
