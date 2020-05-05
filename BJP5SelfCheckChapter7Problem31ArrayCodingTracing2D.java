3182e043be20df1b27a4ed3e7decd5909cd11031
Sarai Hannah Ajai <ajai.sarai.h@outlook.com>
2020-05-05 14:04:52 -0500

    Create BJP5SelfCheckChapter7Problem31ArrayCodingTracing2D.java
    Object Oriented Programming in Java (Microsoft Dev 277x) course from Instructor, Kasey Champion's Practice-It! Assignment for BJP4 Self-Check 7.31 Array Coding Tracing 2D Completed by Sarai Hannah Ajai Dated on May 04, 2020.

---
+++ BJP5SelfCheckChapter7Problem31ArrayCodingTracing2D.java
@@ -0,0 +1,31 @@
+/* Object Oriented Programming in Java (Microsoft Dev 277x) course from Instructor, Kasey Champion's
+   Practice-It! Assignment for BJP4 Self-Check 7.31 Array Coding Tracing 2D.
+   Completed by Sarai Hannah Ajai Dated on May 04, 2020. */
+
+
+public class BJP5SelfCheckChapter7Problem31ArrayCodingTracing2D {
+    public static void main(String [] args){
+        int [][] numbers = new int[3][4];
+        numbers[0][0] = 1;
+        numbers[0][1] = 2;
+        numbers[0][2] = 3;
+        numbers[0][3] = 4;
+        numbers[1][0] = 1;
+        numbers[1][1] = 2;
+        numbers[1][2] = 3;
+        numbers[1][3] = 4;
+        numbers[2][0] = 1;
+        numbers[2][1] = 2;
+        numbers[2][2] = 3;
+        numbers[2][3] = 4;
+
+        for (int r = 0; r < numbers.length; r++) {
+            for (int c = 0; c < numbers[0].length; c++) {
+
+                numbers[r][c] = r + c;
+                System.out.println(numbers[r][c] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
