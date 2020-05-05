d8bdd9073cc8a911575b7769ae29453695c42812
Sarai Hannah Ajai <ajai.sarai.h@outlook.com>
2020-05-05 00:13:07 -0500

    Create Module1Meet2DArraysQuestion2.java
    Module 1 | Meet 2D Arrays Question 2
    Completed by: Sarai Hannah Ajai Dated on May 5, 2020

---
+++ Module1Meet2DArraysQuestion2.java
@@ -0,0 +1,18 @@
+// Module 1 | Meet 2D Arrays Question 2
+// Completed by: Sarai Hannah Ajai
+// on May 5, 2020
+
+import java.util.Arrays;
+
+public class Module1Meet2DArraysQuestion2 {
+    public static void main(String []args) {
+        char[][] arr = new char[5][5];
+        arr[4][0] = 'x';
+        arr[4][1] = 'x';
+        arr[4][2] = 'x';
+        arr[4][3] = 'x';
+        arr[4][4] = 'x';
+
+        System.out.println(arr[4][4] = 'x');
+    }
+}
