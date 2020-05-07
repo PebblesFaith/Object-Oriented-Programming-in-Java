707c506a624cb5eacc8fa6380be2f907f045a847
Sarai Hannah Ajai <ajai.sarai.h@outlook.com>
2020-05-07 13:49:11 -0500

    Update BJP5SelfCheckChapter10Problem17ArrayListMystery3.java

--- BJP5SelfCheckChapter10Problem17ArrayListMystery3.java
+++ BJP5SelfCheckChapter10Problem17ArrayListMystery3.java
@@ -8,18 +8,18 @@ import java.util.ArrayList;
 public class BJP5SelfCheckChapter10Problem17ArrayListMystery3 {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<Integer>();
-        /*list.add(72);
+        list.add(72);
         list.add(20);
-        list.add(1);
+        /*list.add(1);
         list.add(2);
         list.add(3);
         list.add(4);
         list.add(5);
-        list.add(6);*/
+        list.add(6);
         list.add(10);
         list.add(20);
         list.add(30);
-        list.add(40);
+        list.add(40);*/
 
         mystery3(list);
     }
