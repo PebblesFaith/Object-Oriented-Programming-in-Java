2ae9675c570e61033e6c97a0e2447ad303ceb40f
Sarai Hannah Ajai <ajai.sarai.h@outlook.com>
2020-05-21 17:17:05 -0500

    Create Module2StateQuestion2App.java
     Module 2 | State Question 2 Quiz
     Completed by: Sarai Hannah Ajai
     Dated on May 21, 2020

---
+++ Module2StateQuestion2App.java
@@ -0,0 +1,12 @@
+/* Module 2 | State Question 2
+   Completed by: Sarai Hannah Ajai
+   Dated on May 21, 2020
+*/
+public class Module2StateQuestion2App {
+    public static void main(String[] args) {
+        Module2StateQuestion2 greeting = new Module2StateQuestion2();
+        greeting.field = "Hello";
+        System.out.println(greeting.field);
+
+    }
+}
