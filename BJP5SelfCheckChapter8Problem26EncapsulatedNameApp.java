ab12d91858a68a788f8a71b196e2671e25dc952b
Sarai Hannah Ajai <ajai.sarai.h@outlook.com>
2020-05-28 18:57:02 -0500

    Create BJP5SelfCheckChapter8Problem26EncapsulatedNameApp.java
    Object Oriented Programming in Java (Microsoft Dev 277x) by course Instructor Kasey Champion's Practice-It! Assignment for BJP5 Self-Check 8.26 Encapsulated Name with Class Object and Client Java File Programs Completed by Sarai Hannah Ajai Dated on May 28, 2020.

---
+++ BJP5SelfCheckChapter8Problem26EncapsulatedNameApp.java
@@ -0,0 +1,14 @@
+/* Object Oriented Programming in Java (Microsoft Dev 277x) course by Instructor Kasey Champion's
+   Practice-It! Assignment for BJP5 Self-Check 8.26 Encapsulated Name with Class Object and Client Java File Programs
+   Completed by Sarai Hannah Ajai Dated on May 28, 2020. */
+
+public class BJP5SelfCheckChapter8Problem26EncapsulatedNameApp {
+    public static void main(String[] args) {
+        BJP5SelfCheckChapter8Problem26EncapsulatedName name1 = new BJP5SelfCheckChapter8Problem26EncapsulatedName();
+        //name1.getFirstName();
+        //name1.getLastName();
+        //name1.getMiddleInitial();
+        System.out.println(" A Name object represents a person's name such as " + name1.getFirstName() + " " + name1.getLastName() +  " " + name1.getMiddleInitial() +".") ;
+
+    }
+}
