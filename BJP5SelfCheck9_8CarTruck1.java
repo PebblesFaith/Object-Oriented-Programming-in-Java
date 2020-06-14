97763bf6394ed4e634500174794186e3f34bdc4a
Sarai Hannah Ajai <ajai.sarai.h@outlook.com>
2020-06-14 15:33:23 -0500

    Create BJP5SelfCheck9_8CarTruck1.java
    Object Oriented Programming in Java (Microsoft Dev 277x) online course from Instructor, Kasey Champion's Practice-It! Assignment for BJP4 Self-Check 9.8 CarTruck Completed by Sarai Hannah Ajai Dated on June 14, 2020.

---
+++ BJP5SelfCheck9_8CarTruck1.java
@@ -0,0 +1,18 @@
+/*
+    Object Oriented Programming in Java (Microsoft Dev 277x) online course from Instructor, Kasey Champion's
+    Practice-It! Assignment for BJP4 Self-Check 9.8 CarTruck.
+    Completed by Sarai Hannah Ajai Dated on June 14, 2020.
+  */
+
+// Consider the following two automobile classes:
+public class BJP5SelfCheck9_8CarTruck1 {
+    public void m1() {
+        System.out.println("car 1");
+    }
+    public void m2() {
+        System.out.println("car 2");
+    }
+    public String toString() {
+        return "vroom";
+    }
+}
