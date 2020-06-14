a7252d389a370223172392350f06ab87fa880be3
Sarai Hannah Ajai <ajai.sarai.h@outlook.com>
2020-06-14 15:33:49 -0500

    Create BJP5SelfCheck9_8MainCarTruck.java
    Object Oriented Programming in Java (Microsoft Dev 277x) online course from Instructor, Kasey Champion's Practice-It! Assignment for BJP4 Self-Check 9.8 CarTruck Completed by Sarai Hannah Ajai Dated on June 14, 2020.

---
+++ BJP5SelfCheck9_8MainCarTruck.java
@@ -0,0 +1,23 @@
+/*
+    Object Oriented Programming in Java (Microsoft Dev 277x) online course from Instructor, Kasey Champion's
+    Practice-It! Assignment for BJP4 Self-Check 9.8 CarTruck.
+    Completed by Sarai Hannah Ajai Dated on June 14, 2020.
+  */
+
+// Given the following declared variables, what is the output from the following statements?
+public class BJP5SelfCheck9_8MainCarTruck {
+    public static void main(String[] args) {
+        BJP5SelfCheck9_8CarTruck1 myCar = new BJP5SelfCheck9_8CarTruck1();
+        BJP5SelfCheck9_8CarTruck2 myTruck = new BJP5SelfCheck9_8CarTruck2();
+
+        System.out.println(myCar);
+        myCar.m1();
+        myCar.m2();
+
+        System.out.println(myTruck);
+        myTruck.m1();
+        myTruck.m2();
+
+
+    }
+}
