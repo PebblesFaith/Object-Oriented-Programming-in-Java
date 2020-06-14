d814f2c845fba6a42144b392262aee56523f6a17
Sarai Hannah Ajai <ajai.sarai.h@outlook.com>
2020-06-14 15:33:39 -0500

    Create BJP5SelfCheck9_8CarTruck2.java
    Object Oriented Programming in Java (Microsoft Dev 277x) online course from Instructor, Kasey Champion's Practice-It! Assignment for BJP4 Self-Check 9.8 CarTruck Completed by Sarai Hannah Ajai Dated on June 14, 2020.

---
+++ BJP5SelfCheck9_8CarTruck2.java
@@ -0,0 +1,12 @@
+/*
+    Object Oriented Programming in Java (Microsoft Dev 277x) online course from Instructor, Kasey Champion's
+    Practice-It! Assignment for BJP4 Self-Check 9.8 CarTruck.
+    Completed by Sarai Hannah Ajai Dated on June 14, 2020.
+  */
+
+// Consider the following Truck automobile class Inheritance branch:
+public class BJP5SelfCheck9_8CarTruck2 extends BJP5SelfCheck9_8CarTruck1 {
+    public void m1() {
+        System.out.println("truck 1");
+    }
+}
